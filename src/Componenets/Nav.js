5c3c601368e19ade0e2618c81d7e1ba0275d0474
Ashik S Nair <aashiq5342@gmail.com>
2022-07-03 23:04:19 +0530

    initial commit

---
+++ src/Componenets/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <nav className='nav-section'>
+        <div className="wrap nav-wrap">
+            
+        </div>
+    </nav>
+  )
+}
+
+export default Nav
