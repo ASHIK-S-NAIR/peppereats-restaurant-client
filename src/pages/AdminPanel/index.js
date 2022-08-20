4d30d0e5a88e3ff78a7d3d06203bd8c1601a1c7c
Ashik S Nair <aashiq5342@gmail.com>
2022-08-20 22:52:48 +0530

    working on adminpanel

--- src/pages/AdminPanel/index.js
+++ src/pages/AdminPanel/index.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 const AdminPanel = () => {
   return (
-    <div>AdminPanel
-    <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id soluta eaque qui non aut asperiores numquam dolores vitae? Veritatis, esse doloremque. Assumenda alias adipisci nihil natus eos corrupti, doloribus ipsa!
-    </p>
-    </div>
+    <section className="adminPanel-section">
+        <div className="container">
+
+        </div>
+    </section>
    
   )
 }
